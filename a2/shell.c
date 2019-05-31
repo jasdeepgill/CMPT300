d8f2dc6f04d818d25d0e070503a52bd43e74ffcd
johnnyd <johnnyd@asb9838nu-e01.csil.sfu.ca>
2019-05-31 15:49:54 -0700

    prototype v1

---
+++ a2/shell.c
@@ -0,0 +1,265 @@
+// Shell starter file
+// You may make any changes to any part of this file.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define COMMAND_LENGTH 1024
+#define NUM_TOKENS (COMMAND_LENGTH / 2 + 1)
+#define MAX_BUF 1024
+#define HISTORY_DEPTH 10
+char history[HISTORY_DEPTH][COMMAND_LENGTH];
+int cmdNum[HISTORY_DEPTH];
+int count = 0;
+_Bool anpersand[HISTORY_DEPTH];
+
+
+/**
+ * Command Input and Processing
+ */
+
+/*
+ * Tokenize the string in 'buff' into 'tokens'.
+ * buff: Character array containing string to tokenize.
+ *       Will be modified: all whitespace replaced with '\0'
+ * tokens: array of pointers of size at least COMMAND_LENGTH/2 + 1.
+ *       Will be modified so tokens[i] points to the i'th token
+ *       in the string buff. All returned tokens will be non-empty.
+ *       NOTE: pointers in tokens[] will all point into buff!
+ *       Ends with a null pointer.
+ * returns: number of tokens.
+ */
+int tokenize_command(char *buff, char *tokens[])
+{
+	int token_count = 0;
+	_Bool in_token = false;
+	int num_chars = strnlen(buff, COMMAND_LENGTH);
+	for (int i = 0; i < num_chars; i++) {
+		switch (buff[i]) {
+		// Handle token delimiters (ends):
+		case ' ':
+		case '\t':
+		case '\n':
+			buff[i] = '\0';
+			in_token = false;
+			break;
+
+		// Handle other characters (may be start)
+		default:
+			if (!in_token) {
+				tokens[token_count] = &buff[i];
+				token_count++;
+				in_token = true;
+			}
+		}
+	}
+	tokens[token_count] = NULL;
+	return token_count;
+}
+
+/**
+ * Read a command from the keyboard into the buffer 'buff' and tokenize it
+ * such that 'tokens[i]' points into 'buff' to the i'th token in the command.
+ * buff: Buffer allocated by the calling code. Must be at least
+ *       COMMAND_LENGTH bytes long.
+ * tokens[]: Array of character pointers which point into 'buff'. Must be at
+ *       least NUM_TOKENS long. Will strip out up to one final '&' token.
+ *       tokens will be NULL terminated (a NULL pointer indicates end of tokens).
+ * in_background: pointer to a boolean variable. Set to true if user entered
+ *       an & as their last token; otherwise set to false.
+ */
+void read_command(char *buff, char *tokens[], _Bool *in_background)
+{
+	*in_background = false;
+
+	// Read input
+	int length = read(STDIN_FILENO, buff, COMMAND_LENGTH-1);
+
+	if (length < 0) {
+		perror("Unable to read command from keyboard. Terminating.\n");
+		exit(-1);
+	}
+
+	// Null terminate and strip \n.
+	buff[length] = '\0';
+	if (buff[strlen(buff) - 1] == '\n') {
+		buff[strlen(buff) - 1] = '\0';
+	}
+
+	// Tokenize (saving original command string)
+	int token_count = tokenize_command(buff, tokens);
+	if (token_count == 0) {
+		return;
+	}
+
+	// Extract if running in background:
+	if (token_count > 0 && strcmp(tokens[token_count - 1], "&") == 0) {
+		*in_background = true;
+		tokens[token_count - 1] = 0;
+	}
+}
+
+void print(char* s)
+{
+	write(STDOUT_FILENO, s, strlen(s));
+}
+
+void reOrderQueue()
+{
+	for (int i = 0; i < count-1; ++i)
+	{
+		cmdNum[i] = cmdNum[i+1];
+		int j = 0;
+		while(history[i][j] != '\0')
+		{
+			history[i][j] = history[i+1][j];
+		}
+		// history[i] = history[i+1];
+		// anpersand[i] = anpersand[i+1];
+	}
+}
+
+// void printHistory()
+// {
+// 	for (int i = 0; i < HISTORY_DEPTH; ++i)
+// 	{
+// 		int j = 0;
+// 		while(history[i][j] != '\0')
+// 		{
+// 			print()
+// 		}
+// 	}
+// }
+
+void queueSave(char* cmd[])
+{
+	if (count >= 10)
+	{
+		reOrderQueue();
+		count--;
+	}
+	count++;
+	print("signals\n");
+	// if (cmd[2][0] == '&')
+	// {
+	// 	anpersand[count-1] = 1;
+	// }
+	cmdNum[count-1] = count;
+	int i = 0;
+	int j = 0;
+	while(cmd[i] != NULL)
+	{
+		print("doubleweed\n");
+		
+		while(cmd[i][j] != '\0')
+		{
+			history[count-1][j] = cmd[i][j];
+			j++;
+		}
+		history[count-1][j] = '\0';
+		j++;
+		i++;
+	}
+	print("weed\n");
+	// history[count-1] = cmd[1];
+}
+
+void hist()
+{
+	for (int i = 0; i < count; ++i)
+	{
+		char stuff[1024];
+		sprintf(stuff, "%d", cmdNum[i]);
+		print(stuff);
+		print("	");
+		print(history[i]);
+		print("\n");
+	}
+}
+
+/**
+ * Main and Execute Commands
+ */
+int main(int argc, char* argv[])
+{
+	char input_buffer[COMMAND_LENGTH];
+	char *tokens[NUM_TOKENS];
+	while (true) {
+
+		size_t size = sizeof(char) * MAX_BUF;
+		char* buf = (char *) malloc(size);
+		char* cwd;
+
+		cwd = getcwd(buf, size);
+		if (cwd == NULL)
+		{
+			perror("WTF\n");
+		}
+		print(cwd);
+		// Get command
+		// Use write because we need to use read() to work with
+		// signals, and read() is incompatible with printf().
+		write(STDOUT_FILENO, "> ", strlen("> "));
+		_Bool in_background = false;
+		read_command(input_buffer, tokens, &in_background);
+
+		// DEBUG: Dump out arguments:
+		for (int i = 0; tokens[i] != NULL; i++) {
+			write(STDOUT_FILENO, "   Token: ", strlen("   Token: "));
+			write(STDOUT_FILENO, tokens[i], strlen(tokens[i]));
+			write(STDOUT_FILENO, "\n", strlen("\n"));
+		}
+		if (in_background) {
+			write(STDOUT_FILENO, "Run in background.", strlen("Run in background."));
+		}
+
+		/**
+		 * Steps For Basic Shell:
+		 * 1. Fork a child process
+		 * 2. Child process invokes execvp() using results in token array.
+		 * 3. If in_background is false, parent waits for
+		 *    child to finish. Otherwise, parent loops back to
+		 *    read_command() again immediately.
+		 */
+		queueSave(tokens);
+		if (strcmp(tokens[0], "exit") == 0)
+		{
+			print("free me boiii\n");
+			free(buf);
+			free(cwd);
+			print("bruh\n");
+			exit(0);
+		}
+		else if (strcmp(tokens[0], "pwd") == 0)
+		{
+			print(cwd);
+		}
+		// else if (strcmp(tokens[0], "cd") == 0)
+		// {
+		// 	if (chdir(tokens[1]) == -1)
+		// 	{
+		// 		perror("WTF You idiot\n");
+		// 	}
+		// }
+		else if (strcmp(tokens[0], "history") == 0)
+		{
+			hist();
+		}
+
+		// execvp(tokens[0], tokens);
+		// if (!in_background)
+		// {
+		// 	waitpid(-1, NULL, 0)
+		// }
+		// else
+		// {
+		// 	while (waitpid(-1, NULL, WNOHANG) > 0); // do nothing.
+		// }
+		print("SWAG\n");
+	}
+
+	return 0;
+}

6dac13ab7015dc8c0e39924bdcc8dde473742813
johnnyd <johnnyd@asb9838nu-e01.csil.sfu.ca>
2019-06-28 11:54:15 -0700

    V1.2.0

--- a3/bbuff.c
+++ a3/bbuff.c
@@ -12,6 +12,7 @@ void* bbuff_blocking_extract(void);
 _Bool bbuff_is_empty(void);
 
 static void* bbuff[BUFFER_SIZE];
+static int tail = 0;
 sem_t mutex;
 sem_t num_items;
 sem_t empty_slots;
@@ -32,13 +33,8 @@ void bbuff_blocking_insert(void* item)
 	
 	sem_wait(&empty_slots);
 	sem_wait(&mutex);
-	// int i = 0;
-	// while(bbuff[i] != NULL) i++;
-	// bbuff[i] = item;
-	// printf("Insert i = %d\n", i);
-	int tail;
-	sem_getvalue(&num_items, &tail);
 	bbuff[tail] = item;
+	tail++;
 	sem_post(&mutex);
 	sem_post(&num_items);
 	
@@ -49,23 +45,11 @@ void* bbuff_blocking_extract(void)
 	
 	sem_wait(&num_items);
 	sem_wait(&mutex);
-	// int i = 0;
-	// while(bbuff[i] == NULL && !bbuff_is_empty()) i++;
-	// printf("Extract i = %d\n", i);
-	// void *temp = malloc(sizeof(void*));
-	// if (temp == NULL)
-	// {
-	// 	printf("Error: Could not allocate in func bbuff_blocking_extract().\n");
-	// 	exit(-1);
-	// }
-	// memcpy(bbuff[i], temp, sizeof(void*));
-	// bbuff[i] = NULL;
-	// printf("Error may be here\n");
-	int tail;
-	sem_getvalue(&num_items, &tail);
+	printf("Extract at %d\n", tail);
 	void *temp = malloc(sizeof(void*));
-	memcpy(bbuff[tail], temp, sizeof(void*));
-	bbuff[tail] = NULL;
+	temp = bbuff[tail-1];
+	bbuff[tail-1] = NULL;
+	tail--;
 	sem_post(&mutex);
 	sem_post(&empty_slots);
 	return temp;
